ca708e60fe66d1936683f0cc269a975c0eb117b7
marcdevceo <marcdevceo@icloud.com>
2025-07-13 11:24:32 -0400

    fix: removed import for mellc-ui-kit/styles/index.css from layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
-import 'mellc-ui-kit/styles/index.css';
 import '../styles/project-colors.css';
 import { ClerkProvider } from '@clerk/nextjs';
 import React from 'react';
